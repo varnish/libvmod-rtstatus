0439d212c8b2d04bca3e71d510e6d26db1f4ca07
arianna-aondio <arianna@arianna-ThinkPad-X230.(none)>
2014-07-03 14:18:02 +0200

    Indentation.

--- src/vmod_rtstatus.c
+++ src/vmod_rtstatus.c
@@ -15,97 +15,96 @@
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 {
-  return (0);
+	return (0);
 }
 /////////////////////////////////////////////////////////
 char *
 director(struct sess *sp, char *p)
 {
-  strcat(p,"\"DIRECTOR\": {\"name\":\"");
-  strcat(p, sp->director->name);
-  strcat(p,"\", \"vcl_name\":\"");
-  strcat(p,sp->director->vcl_name);
-  strcat(p, "\"},\n");
-  return p;
+	strcat(p,"\"DIRECTOR\": {\"name\":\"");
+	strcat(p, sp->director->name);
+	strcat(p,"\", \"vcl_name\":\"");
+	strcat(p,sp->director->vcl_name);
+	strcat(p, "\"},\n");
+	return p;
 }
 /////////////////////////////////////////////////////////
 int
 json_status(void *priv, const struct VSC_point *const pt)
 {
-  char tmp[1024];
-  int i;
-  uint64_t val;
+	char tmp[1024];
+	int i;
+	uint64_t val;
+	val=*(const volatile uint64_t*)pt->ptr;
+	i = 0;
+	strcat(priv,"\"");
+	if (strcmp(pt->class, "")) {
+		i += strcat(priv, pt->class);
+		strcat(priv, ".");
+	}
+	if (strcmp(pt->ident, "")) {
+		i += strcat(priv, pt->ident);
+		strcat(priv,".");
+	}
+	i += strcat(priv, pt->name);
+	sprintf(tmp,"\": {");
+	strcat(priv,tmp);
 
-  val=*(const volatile uint64_t*)pt->ptr;
-  i = 0;
-  strcat(priv,"\"");
-  if (strcmp(pt->class, "")) {
-    i += strcat(priv, pt->class);
-    strcat(priv, ".");
-  }
-  if (strcmp(pt->ident, "")) {
-    i += strcat(priv, pt->ident);
-    strcat(priv,".");
-  }
-  i += strcat(priv, pt->name);
-  sprintf(tmp,"\": {");
-  strcat(priv,tmp);
-
-  if (strcmp(pt->class, "")) {
-    sprintf(tmp,"type\": \"" );
-    strcat(priv,tmp);
-    strcat(priv,pt->class);
-    strcat(priv,"\", ");
-  }
-  if (strcmp(pt->ident, "")) {
-    sprintf(tmp,"\"ident\": \"" );
-    strcat(priv,tmp);
-    strcat(priv,pt->ident);
-    strcat(priv,"\", ");
-  }
-  sprintf(tmp,"\"descr\": \"" );
-  strcat(priv,tmp);
-  strcat(priv,pt->desc);
-  strcat(priv,"\", ");
-  sprintf(tmp,"\"value\": \"%d\"},\n",val );
-  strcat(priv,tmp);
-  return (0);
+	if (strcmp(pt->class, "")) {
+		sprintf(tmp,"type\": \"" );
+		strcat(priv,tmp);
+		strcat(priv,pt->class);
+		strcat(priv,"\", ");
+	}
+	if (strcmp(pt->ident, "")) {
+		sprintf(tmp,"\"ident\": \"" );
+		strcat(priv,tmp);
+		strcat(priv,pt->ident);
+		strcat(priv,"\", ");
+	}
+	sprintf(tmp,"\"descr\": \"" );
+	strcat(priv,tmp);
+	strcat(priv,pt->desc);
+	strcat(priv,"\", ");
+	sprintf(tmp,"\"value\": \"%d\"},\n",val );
+	strcat(priv,tmp);
+	return (0);
 }
 ///////////////////////////////////////////////////////
 const char*
 vmod_rtstatus(struct sess *sp)
 {
-  char *p;
-  unsigned max_sz;
-  char time_stamp[22];
-  time_t now;
-  struct VSM_data *vd;
-  const struct VSC_C_main *VSC_C_main;
+	char *p;
+	unsigned max_sz;
+	char time_stamp[22];
+	time_t now;
+	struct VSM_data *vd;
+	const struct VSC_C_main *VSC_C_main;
 
-  max_sz = WS_Reserve(sp->wrk->ws, 0);
-  p = sp->wrk->ws->f;
-  *p = 0;
+	max_sz = WS_Reserve(sp->wrk->ws, 0);
+	p = sp->wrk->ws->f;
+	*p = 0;
 
-  vd = VSM_New();
-  VSC_Setup(vd);
+	vd = VSM_New();
+	VSC_Setup(vd);
 
-  if (VSC_Open(vd, 1))
-    exit(1);
-  VSC_C_main = VSC_Main(vd);
-  strcat(p,"{\n");
-  now = time(NULL);
-  (void)strftime(time_stamp, 22, "%Y-%m-%d T %H:%M:%S", localtime(&now));
-  strcat(p,"\"Timestamp\" : ");
-  strcat(p,time_stamp);
-  strcat(p,"\n\n");
-  director(sp,p);
-  (void)VSC_Iter(vd, json_status,(void *)p);
-  strcat(p, "}\n");
-  if (strlen(p)>=max_sz) {
-    WS_Release(sp->wrk->ws, 0);
-    WSL(sp->wrk, SLT_Error, sp->fd, "Running out of workspace in vmod_rtstatus. Increase sess_workspace to fix this.");
-    return ""; 
-  }
-  WS_Release(sp->wrk->ws, strlen(p));
-  return (p);
+	if (VSC_Open(vd, 1))
+		exit(1);
+	VSC_C_main = VSC_Main(vd);
+	strcat(p,"{\n");
+	now = time(NULL);
+	(void)strftime(time_stamp, 22, "%Y-%m-%d T %H:%M:%S", localtime(&now));
+	strcat(p,"\"Timestamp\" : ");
+	strcat(p,time_stamp);
+	strcat(p,"\n\n");
+	director(sp,p);
+	(void)VSC_Iter(vd, json_status,(void *)p);
+	strcat(p, "}\n");
+	if (strlen(p)>=max_sz) {
+		WS_Release(sp->wrk->ws, 0);
+		WSL(sp->wrk, SLT_Error, sp->fd, "Running out of workspace in vmod_rtstatus. Increase sess_workspace to fix this.");
+		return ""; 
+	}
+	WS_Release(sp->wrk->ws, strlen(p));
+	return (p);
 }
