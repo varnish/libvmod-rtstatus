3dcf9368e2bd1c8a0adcd630825b78c650a9c8b2
arianna-aondio <arianna@arianna-ThinkPad-X230.(none)>
2014-07-07 10:49:31 +0200

    Warnings fixed.

--- src/vmod_rtstatus.c
+++ src/vmod_rtstatus.c
@@ -21,7 +21,7 @@
 	    WSL(sp->wrk, SLT_Error, sp->fd,				\
 		"Running out of workspace in vmod_backendhealth. "	\
 		"Increase sess_workspace to fix this.");		\
-	    return "";							\
+	    return 1;							\
 	}								\
     } while(0)								\
 
@@ -31,7 +31,7 @@ struct iter_priv{
     unsigned ws_sz;
     struct sess *cpy_sp;
 };
-
+//aggiungi un pointer e ridifinisci wssstrincat
 
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
@@ -48,15 +48,21 @@ wsstrncat(char *dest, const char *src, unsigned max_sz) {
     return strcat(dest, src);
 }
 /////////////////////////////////////////////////////////
-char *
+int
 director(struct sess *sp, char *p, unsigned max_sz)
 {
+    char *time_stamp;
+    time_stamp = VRT_time_string(sp, sp->t_req);
+   
+    STRCAT(p, "\"Timestamp\" : ",max_sz, sp);
+    STRCAT(p, time_stamp,max_sz,sp);
+    STRCAT(p, "\n\n",max_sz,sp);
     STRCAT(p,"\"DIRECTOR\": {\"name\":\"",max_sz,sp);
     STRCAT(p, sp->director->name,max_sz,sp);
     STRCAT(p,"\", \"vcl_name\":\"",max_sz,sp);
     STRCAT(p,sp->director->vcl_name,max_sz,sp);
     STRCAT(p, "\"},\n",max_sz,sp);
-    return p;
+    return 0;
 }
 /////////////////////////////////////////////////////////
 int
@@ -94,7 +100,7 @@ json_status(void *priv, const struct VSC_point *const pt)
     sprintf(tmp, "\"value\": \"%" PRIu64 "},\n", val );
     STRCAT(iter->p, tmp, iter->ws_sz, iter->cpy_sp);
 
-    return (0);
+    return 0;
 }
 ///////////////////////////////////////////////////////
 const char*
@@ -102,19 +108,17 @@ vmod_rtstatus(struct sess *sp)
 {
     struct iter_priv iter = { 0 };
     unsigned max_sz;
-    char *time_stamp;
     struct tm t_time;
     struct VSM_data *vd;
     const struct VSC_C_main *VSC_C_main;
  
-    time_stamp = VRT_time_string(sp, sp->t_req);
-
+    
     vd = VSM_New();
     VSC_Setup(vd);
     
     if (VSC_Open(vd, 1)) {
 	WSL(sp->wrk, SLT_Error, sp->fd,"VSC can't be opened.");
-	return; 
+	return ""; 
     }
     max_sz = WS_Reserve(sp->wrk->ws, 0);
     iter.p = sp->wrk->ws->f;
@@ -122,13 +126,9 @@ vmod_rtstatus(struct sess *sp)
     iter.ws_sz = WS_Free(sp->wrk->ws);
     iter.cpy_sp = sp;
     VSC_C_main = VSC_Main(vd);
-    STRCAT(iter.p, "{\n", max_sz, sp);
-    STRCAT(iter.p, "\"Timestamp\" : ",max_sz, sp);
-    STRCAT(iter.p, time_stamp,max_sz,sp);
-    STRCAT(iter.p, "\n\n",max_sz,sp);
     director(sp, iter.p, max_sz);
     (void)VSC_Iter(vd, json_status, &iter);
-    STRCAT(iter.p, "}\n",max_sz,sp);
+    
     VSM_Delete(vd);
     WS_Release(sp->wrk->ws, strlen(iter.p) + 1);
     
