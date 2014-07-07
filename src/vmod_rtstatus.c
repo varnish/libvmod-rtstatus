ed6646eb59a7e4c23b92dbb73fb17053e996515c
arianna-aondio <arianna@arianna-ThinkPad-X230.(none)>
2014-07-07 16:47:21 +0200

    Work in progress.

--- src/vmod_rtstatus.c
+++ src/vmod_rtstatus.c
@@ -29,6 +29,7 @@
 struct iter_priv{
     char *p;
     struct sess *cpy_sp;
+    char *time_stamp;
 };
 
 int
@@ -48,11 +49,9 @@ wsstrncat(char *dest, const char *src, struct sess *sp) {
 int
 director(struct iter_priv *iter)
 {
-    char *time_stamp;
-    time_stamp = VRT_time_string(iter->cpy_sp,iter->cpy_sp->t_req);
-   
+    
     STRCAT(iter->p, "\"Timestamp\" : ", iter->cpy_sp);
-    STRCAT(iter->p, time_stamp,iter->cpy_sp);
+    STRCAT(iter->p,iter->time_stamp,iter->cpy_sp);
     STRCAT(iter->p, "\n\n", iter->cpy_sp);
     STRCAT(iter->p,"\"DIRECTOR\": {\"name\":\"",iter->cpy_sp);
     STRCAT(iter->p, iter->cpy_sp->director->name, iter->cpy_sp);
@@ -108,14 +107,17 @@ vmod_rtstatus(struct sess *sp)
     struct tm t_time;
     struct VSM_data *vd;
     const struct VSC_C_main *VSC_C_main;
-     
+    char *time_stamp;
+    
     vd = VSM_New();
     VSC_Setup(vd);
     
     if (VSC_Open(vd, 1)) {
 	WSL(sp->wrk, SLT_Error, sp->fd, "VSC can't be opened.");
-	return "error"; 
+	VSM_Delete(vd);
+	return ""; 
     }
+    iter.time_stamp = VRT_time_string(iter.cpy_sp,iter.cpy_sp->t_req);
     WS_Reserve(sp->wrk->ws, 0);
     iter.p = sp->wrk->ws->f;
     *(iter.p) = 0;
