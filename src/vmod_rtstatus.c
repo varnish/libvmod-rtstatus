1019568de9ca21413f2fe2cac299c398174318ce
Arianna Aondio <arianna.aondio@varnish-software.com>
2014-09-03 13:01:32 +0200

    Added new source file.

--- src/vmod_rtstatus.c
+++ src/vmod_rtstatus.c
@@ -6,28 +6,20 @@
 #include <string.h>
 #include <unistd.h>
 #include <math.h>
+
+#include "miniobj.h"
 #include "vrt.h"
 #include "vrt_obj.h"
-
-#include "vcc_if.h"
-//#include "libvarnishapi.h"
-//#include "cache/cache.h"
 #include "vapi/vsc.h"
-
 #include "vapi/vsm.h"
-//#include "vtim.h"
-#include "vcl.h"
-//#include "cache/cache_backend.h"
+#include "vmod_rtstatus.h"
 
-struct ws {
-	unsigned		magic;
-#define WS_MAGIC		0x35fac554
-	char			id[4];		/* identity */
-	char			*s;		/* (S)tart of buffer */
-	char			*f;		/* (F)ree/front pointer */
-	char			*r;		/* (R)eserved length */
-	char			*e;		/* (E)nd of buffer */
-};
+int
+init_function (struct vmod_priv *priv, const struct VCL_conf *conf)
+{
+return (0);
+}
+
 #define STRCAT(dst, src, ctx)					\
     do {							\
 	dst = wsstrncat(dst, src, ctx);				\
@@ -44,11 +36,18 @@ struct iter_priv{
     struct vtc_job *jp;
     double time;
 };
-int
-init_function (struct vmod_priv *priv, const struct VCL_conf *conf)
-{
-return (0);
-}
+
+
+
+struct ws {
+	unsigned		magic;
+#define WS_MAGIC		0x35fac554
+	char			id[4];		/* identity */
+	char			*s;		/* (S)tart of buffer */
+	char			*f;		/* (F)ree/front pointer */
+	char			*r;		/* (R)eserved length */
+	char			*e;		/* (E)nd of buffer */
+};
 //////////////////////////////////////////////////////////
 static char *
 wsstrncat (char *dest, const char *src, const struct vrt_ctx *ctx)
@@ -59,65 +58,7 @@ wsstrncat (char *dest, const char *src, const struct vrt_ctx *ctx)
 	}
     return strcat (dest, src);
 }
-/////////////////////////////////////////////////////////
-int
-general_info (struct iter_priv *iter)
-{
-char tmp[128];
-/*STRCAT (iter->p, "\t\"req_request\": \"", iter->cpy_ctx);
-STRCAT (iter->p, VRT_r_req(iter->cpy_ctx), iter->cpy_ctx);
-STRCAT (iter->p, "\",\n", iter->cpy_ctx);*/
-//STRCAT (iter->p, VRT_r_obj_status (iter->cpy_ctx) , iter->cpy_ctx);
-sprintf (tmp, "\t\"timestamp\": %f,\n", iter->time);
-STRCAT (iter->p, tmp, iter->cpy_ctx);
-STRCAT (iter->p, "\t\"timestamp\" : \"", iter->cpy_ctx);
-STRCAT (iter->p, iter->time_stamp, iter->cpy_ctx);
-//STRCAT (iter->p, "\",\n\t\"varnish_version\" : \"", iter->cpy_ctx);
-//STRCAT (iter->p, VCS_version, iter->cpy_ctx);
-//STRCAT (iter->p, "\",\n", iter->cpy_ctx);
-/*sprintf (tmp, "\t\"varnish_port\": %d,\n", VRT_r_server_hostname (iter->cpy_ctx));
-  STRCAT (iter->p, tmp, iter->cpy_ctx);*/
-STRCAT (iter->p, "\t\"server_id\": \"", iter->cpy_ctx);
-STRCAT (iter->p, VRT_r_server_identity (iter->cpy_ctx), iter->cpy_ctx);
-STRCAT (iter->p, "\",\n", iter->cpy_ctx);
-STRCAT (iter->p, "\t\"client_id\": \"", iter->cpy_ctx);
-STRCAT (iter->p, VRT_r_client_identity (iter->cpy_ctx), iter->cpy_ctx);
-STRCAT (iter->p, "\",\n", iter->cpy_ctx);
 
-
-//STRCAT (iter->p, VRT_r_beresp_backend_name(iter->cpy_ctx), iter->cpy_ctx);
-
-return (0);
-}
-//////////////////////////////////////////////////////////
-/*int
-backend (struct iter_priv *iter)
-{
-    int i;
-    int cont = 1;
-    STRCAT (iter->p, "\t\"backend\": [", iter->cpy_ctx);
-    for (i = 1; i < iter->cpy_ctx->vcl->ndirector; ++i)
-	{
-	    CHECK_OBJ_NOTNULL (iter->cpy_ctx->vcl->director[i], DIRECTOR_MAGIC);
-	    if (strcmp ("simple", iter->cpy_ctx->vcl->director[i]->name) == 0)
-		{
-		    char buf[1024];
-		    int j, healthy;
-		    healthy = VDI_Healthy (iter->cpy_ctx->vcl->director[i]);
-		    j = snprintf (buf, sizeof buf, "{\"name\":\"%s\", \"value\": \"%s\"}",
-				  iter->cpy_ctx->vcl->director[i]->vcl_name,
-				  healthy ? "healthy" : "sick");
-		    assert (j >= 0);
-		    STRCAT (iter->p, buf, iter->cpy_ctx);
-		    if (i < (iter->cpy_ctx->vcl->ndirector - 2))
-			{
-			    STRCAT (iter->p, ",", iter->cpy_ctx);
-			}
-		}
-	}
-    STRCAT (iter->p, "],\n", iter->cpy_ctx);
-    return (0);
-    }*/
 ///////////////////////////////////////////////////////
 int
 rate (struct iter_priv *iter,struct VSM_data *vd )
@@ -200,18 +141,7 @@ json_status (void *priv, const struct VSC_point *const pt)
     STRCAT (iter->p, "\n", iter->cpy_ctx);
     return (0);
     }
-///////////////////////////////////////////////////////
-int
-run_subroutine (struct iter_priv *iter, struct VSM_data *vd)
-{
-    STRCAT (iter->p, "{\n", iter->cpy_ctx);
-    rate (iter,vd);
-    general_info (iter);
-    //backend (iter);
-    (void)VSC_Iter(vd, NULL, json_status, iter);
-    STRCAT (iter->p, "\n}\n", iter->cpy_ctx);
-    return (0);
-    }
+
 ///////////////////////////////////////////////////////
 VCL_STRING
 vmod_rtstatus (const struct vrt_ctx *ctx)
@@ -235,8 +165,9 @@ vmod_rtstatus (const struct vrt_ctx *ctx)
     *(iter.p) = 0;
     iter.cpy_ctx = ctx;
     VSC_C_main =  VSC_Main(vd,NULL);
-    run_subroutine (&iter,vd);
-   
+rate(&iter,vd);
+   (void)VSC_Iter(vd, NULL, json_status, &iter);
+  run_subroutine (&iter,vd);
    VSM_Delete (vd);
     WS_Release (ctx->ws, strlen (iter.p) + 1);
     return (iter.p);
