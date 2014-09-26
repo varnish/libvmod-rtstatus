17f709e7d3d29466a288c26b5ec5317287af86f2
Arianna Aondio <arianna.aondio@varnish-software.com>
2014-09-26 14:03:46 +0200

    Indentation.

--- src/vmod_rtstatus.c
+++ src/vmod_rtstatus.c
@@ -26,7 +26,6 @@ struct ws {
 	char			*r;		/* (R)eserved length */
 	char			*e;		/* (E)nd of buffer */
 };
-
 ///////////////////////////////////////////////////////
 int
 rate(struct iter_priv *iter,struct VSM_data *vd )
@@ -54,11 +53,11 @@ rate(struct iter_priv *iter,struct VSM_data *vd )
 	up = VSC_C_main->uptime;
 	sprintf(tmp, "\t\"uptime\" : %.0f,\n", up);
 	STRCAT(iter->p, tmp, iter->cpy_ctx);
-	sprintf (tmp, "\t\"hitrate\": %.2f,\n", ratio);
-	STRCAT (iter->p, tmp, iter->cpy_ctx);
-	sprintf (tmp, "\t\"load\": %.0f,\n", (VSC_C_main->client_req / up));
-	STRCAT (iter->p, tmp, iter->cpy_ctx);
-	return (0);
+	sprintf(tmp, "\t\"hitrate\": %.2f,\n", ratio);
+	STRCAT(iter->p, tmp, iter->cpy_ctx);
+	sprintf(tmp, "\t\"load\": %.0f,\n", (VSC_C_main->client_req / up));
+	STRCAT(iter->p, tmp, iter->cpy_ctx);
+	return(0);
 }
 /////////////////////////////////////////////////////////
 int
@@ -78,40 +77,37 @@ json_status(void *priv, const struct VSC_point *const pt)
 	if (iter->jp)
 		iter->jp = 0;
 	else
-		STRCAT (iter->p, ",\n", iter->cpy_ctx);
-	STRCAT (iter->p, "\t\"", iter->cpy_ctx);
-	if (strcmp (sec->fantom->type, "")) {
-		STRCAT (iter->p, sec->fantom->type, iter->cpy_ctx);
-		STRCAT (iter->p, ".", iter->cpy_ctx);
+		STRCAT(iter->p, ",\n", iter->cpy_ctx);
+	STRCAT(iter->p, "\t\"", iter->cpy_ctx);
+	if (strcmp(sec->fantom->type, "")) {
+		STRCAT(iter->p, sec->fantom->type, iter->cpy_ctx);
+		STRCAT(iter->p, ".", iter->cpy_ctx);
 	}
-	if (strcmp (sec->fantom->ident, "")) {
-		STRCAT (iter->p, sec->fantom->ident, iter->cpy_ctx);
-		STRCAT (iter->p, ".", iter->cpy_ctx);
+	if (strcmp(sec->fantom->ident, "")) {
+		STRCAT(iter->p, sec->fantom->ident, iter->cpy_ctx);
+		STRCAT(iter->p, ".", iter->cpy_ctx);
 	}
-	STRCAT (iter->p, pt->desc->name, iter->cpy_ctx);
-	STRCAT (iter->p, "\": {", iter->cpy_ctx);
-	if (strcmp (sec->fantom->type, "")) {
-		STRCAT (iter->p, "\"type\": \"", iter->cpy_ctx);
-		STRCAT (iter->p, sec->fantom->type, iter->cpy_ctx);
-		STRCAT (iter->p, "\", ", iter->cpy_ctx);
+	STRCAT(iter->p, pt->desc->name, iter->cpy_ctx);
+	STRCAT(iter->p, "\": {", iter->cpy_ctx);
+	if (strcmp(sec->fantom->type, "")) {
+		STRCAT(iter->p, "\"type\": \"", iter->cpy_ctx);
+		STRCAT(iter->p, sec->fantom->type, iter->cpy_ctx);
+		STRCAT(iter->p, "\", ", iter->cpy_ctx);
 	}
-	if (strcmp (sec->fantom->ident, "")) {
-		STRCAT (iter->p, "\"ident\": \"", iter->cpy_ctx);
-		STRCAT (iter->p, sec->fantom->ident, iter->cpy_ctx);
-		STRCAT (iter->p, "\", ", iter->cpy_ctx);
+	if (strcmp(sec->fantom->ident, "")) {
+		STRCAT(iter->p, "\"ident\": \"", iter->cpy_ctx);
+		STRCAT(iter->p, sec->fantom->ident, iter->cpy_ctx);
+		STRCAT(iter->p, "\", ", iter->cpy_ctx);
 	}
-	STRCAT (iter->p, "\"descr\": \"", iter->cpy_ctx);
-	STRCAT (iter->p, pt->desc->sdesc, iter->cpy_ctx);
-	STRCAT (iter->p, "\", ", iter->cpy_ctx);
-	sprintf (tmp, "\"value\": %" PRIu64 "}", val);
-	STRCAT (iter->p, tmp, iter->cpy_ctx);
+	STRCAT(iter->p, "\"descr\": \"", iter->cpy_ctx);
+	STRCAT(iter->p, pt->desc->sdesc, iter->cpy_ctx);
+	STRCAT(iter->p, "\", ", iter->cpy_ctx);
+	sprintf(tmp, "\"value\": %" PRIu64 "}", val);
+	STRCAT(iter->p, tmp, iter->cpy_ctx);
 	if (iter->jp)
-		STRCAT (iter->p, "\n", iter->cpy_ctx);
-	return (0);
+		STRCAT(iter->p, "\n", iter->cpy_ctx);
+	return(0);
 }
-
-
-
 ////////////////////////////////////////////////////////
 int
 run_subroutine(struct iter_priv *iter, struct VSM_data *vd)
@@ -122,8 +118,6 @@ run_subroutine(struct iter_priv *iter, struct VSM_data *vd)
 	backend(iter);
 	return(0);
 }
-
-
 ///////////////////////////////////////////////////////
 VCL_STRING
 vmod_rtstatus(const struct vrt_ctx *ctx)
@@ -134,25 +128,25 @@ vmod_rtstatus(const struct vrt_ctx *ctx)
 
 	const struct VSC_C_main *VSC_C_main;
 	vd = VSM_New();
-	
+
 	if (VSM_Open(vd)) {
 	    VSM_Error(vd);
-	    VSM_Delete (vd);
+	    VSM_Delete(vd);
 	    return "";
 	}
-	WS_Reserve (ctx->ws, 0);
+	WS_Reserve(ctx->ws, 0);
 	iter.p = ctx->ws->f;
 	*(iter.p) = 0;
 	iter.cpy_ctx = ctx;
 
 	iter.jp = 1;
-	VSC_C_main =  VSC_Main(vd,NULL);
+	VSC_C_main = VSC_Main(vd, NULL);
 
-	run_subroutine(&iter,vd);
+	run_subroutine(&iter, vd);
 	(void)VSC_Iter(vd, NULL, json_status, &iter);
-	STRCAT(iter.p, "\n}\n",iter.cpy_ctx);
+	STRCAT(iter.p, "\n}\n", iter.cpy_ctx);
 
 	VSM_Delete(vd);
-	WS_Release (ctx->ws, strlen (iter.p) + 1);
+	WS_Release(ctx->ws, strlen(iter.p) + 1);
 	return(iter.p);
 }
