d7f531790ca8b7c09503a282446765c9741948be
Arianna Aondio <arianna.aondio@varnish-software.com>
2014-09-26 13:56:29 +0200

    Indentation.

--- src/vmod_rtstatus.h
+++ src/vmod_rtstatus.h
@@ -1,23 +1,18 @@
+char *wsstrncat(char *dest, const char *src, const struct vrt_ctx *ctx);
+
 struct iter_priv{
-    char *p;
-    const struct vrt_ctx *cpy_ctx;
-   	int jp;
-   // double time;
+	char *p;
+	const struct vrt_ctx *cpy_ctx;
+	int jp;
 };
 
-
-
-
 #define STRCAT(dst, src, ctx)					\
-    do {							\
-	dst = wsstrncat(dst, src, ctx);				\
-	if (!dst) {						\
-	    WS_Release(ctx->ws, 0);				\
-	    return 1;						\
-	}							\
-    } while(0)							\
+	do {							\
+		dst = wsstrncat(dst, src, ctx);			\
+			if (!dst) {				\
+				WS_Release(ctx->ws, 0);		\
+				return 1;			\
+			}					\
+	} while(0)						\
 
-
-/////////////////////////////////////////////////////////
-char *wsstrncat(char *dest, const char *src, const struct vrt_ctx *ctx);
 
