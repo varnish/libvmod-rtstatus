99463212a6c68cf296d59c0765be211de0d45064
Arianna Aondio <arianna.aondio@varnish-software.com>
2014-10-03 15:21:09 +0200

    New subroutine for html() and possible new features.

--- src/vmod_html.c
+++ src/vmod_html.c
@@ -11,13 +11,20 @@
 #include "vmod_rtstatus.h"
 #include "vmod_html.h"
 
+int
+print_html( char *p, const struct vrt_ctx *ctx)
+{
+	STRCAT(p, html, ctx);
+	return(0);
+}
+///////////////////////////////////////////////
 VCL_STRING
 vmod_html(const struct vrt_ctx *ctx)
 {
 	char *p;
 	WS_Reserve(ctx->ws, 0);
 	p = ctx->ws->f;
-	STRCAT(p, html, ctx);
+	print_html(p, ctx);
 	WS_Release(ctx->ws, strlen(p) + 1);
 	return (p);
 }
