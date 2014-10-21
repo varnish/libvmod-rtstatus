ed5aea083434d3c4d225013b30625c2c2e317cdb
arianna-aondio <arianna.aondio@varnish-software.com>
2014-10-21 11:13:35 +0200

    Update vmod_html.js

--- src/vmod_html.js
+++ src/vmod_html.js
@@ -1,5 +1,5 @@
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js">
-
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
+<script type="text/javascript">
 
   function openSpoiler(id){
   var a = document.getElementById(id).style;
