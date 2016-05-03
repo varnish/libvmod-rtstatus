b52713ef2401ba33f17e4ace489a660ea034103c
vagrant <vagrant@ubuntu-14.04-amd64.local>
2016-05-03 08:16:10 +0000

    Change name to a struct to have a more readable code: from iter_priv to rtsatus_priv

--- src/HTML/vmod_html.c
+++ src/HTML/vmod_html.c
@@ -7,16 +7,16 @@
 VCL_STRING
 vmod_html(const struct vrt_ctx *ctx)
 {
-	struct iter_priv iter = { 0 };
-	iter.vsb = VSB_new(NULL, ctx->ws->f, WS_Reserve(ctx->ws, 0), VSB_AUTOEXTEND); 
-	VSB_cat(iter.vsb, html);
-	VSB_finish(iter.vsb);
-	if (VSB_error(iter.vsb)) {
+	struct rtstatus_priv rtstatus = { 0 };
+	rtstatus.vsb = VSB_new(NULL, ctx->ws->f, WS_Reserve(ctx->ws, 0), VSB_AUTOEXTEND);
+	VSB_cat(rtstatus.vsb, html);
+	VSB_finish(rtstatus.vsb);
+	if (VSB_error(rtstatus.vsb)) {
 	    VSLb(ctx->vsl, SLT_VCL_Error, "VSB error");
-	    WS_Release(ctx->ws, VSB_len(iter.vsb) + 1);
+	    WS_Release(ctx->ws, VSB_len(rtstatus.vsb) + 1);
 	    return "{}";
 	}
-	WS_Release(ctx->ws, VSB_len(iter.vsb) + 1);
-	return (iter.vsb->s_buf);
+	WS_Release(ctx->ws, VSB_len(rtstatus.vsb) + 1);
+	return (rtstatus.vsb->s_buf);
 }
 
