c02d69d06615f86030dc3afde6fed099e0d53394
Arianna Aondio <arianna.aondio@varnish-software.com>
2016-05-27 12:07:18 +0000

    Use WS_Reserve and WS_Release instead of VSB

--- src/HTML/vmod_html.c
+++ src/HTML/vmod_html.c
@@ -1,7 +1,6 @@
 #include "cache/cache.h"
 #include "vrt.h"
 #include "vcl.h"
-#include "vsb.h"
 
 #include "vmod_rtstatus.h"
 #include "vmod_html.h"
@@ -9,22 +8,23 @@
 VCL_STRING
 vmod_html(const struct vrt_ctx *ctx)
 {
-	struct vsb *v;
+	char *p;
+	unsigned u,v;
 
         if (ctx->method != VCL_MET_SYNTH) {
                 VSLb(ctx->vsl, SLT_VCL_Error, "rtstatus() can only be used in vcl_synth");
                 return "{ \"error\": \"Check Varnishlog for more details\" }";
         }
 
-	v = VSB_new(NULL, ctx->ws->f, WS_Reserve(ctx->ws, 0), VSB_AUTOEXTEND);
-	VSB_cat(v, html);
-	VSB_finish(v);
-	if (VSB_error(v)) {
-	    VSLb(ctx->vsl, SLT_VCL_Error, "VSB error");
-	    WS_Release(ctx->ws, VSB_len(v) + 1);
-	    return "{ \"error\": \"Check Varnishlog for more details\" }";
+	u = WS_Reserve(ctx->ws, 0);
+	p = ctx->ws->f;
+	v = snprintf(p, u, "%s", html);
+	v++;
+	if (v > u) {
+	    WS_Release(ctx->ws, 0);
+	    return "{ \"error\": \"Workspace overflow\" }";
 	}
-	WS_Release(ctx->ws, VSB_len(v) + 1);
-	return (v->s_buf);
+	WS_Release(ctx->ws, v);
+	return (p);
 }
 
