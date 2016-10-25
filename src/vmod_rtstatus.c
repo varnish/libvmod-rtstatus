3cac2d786eb324f74a9384db63773494bebbe39b
Lasse Karstensen <lkarsten@varnish-software.com>
2016-10-25 13:22:39 +0200

    Retire superfluous source file.
    Makes more sense to have the two public functions in the same file.

--- src/vmod_rtstatus.c
+++ src/vmod_rtstatus.c
@@ -5,6 +5,7 @@
 #include "vrt.h"
 #include "vrt_obj.h"
 
+#include "vmod_rtstatus_html.h"
 #include "vcc_rtstatus_if.h"
 
 VCL_STRING
@@ -36,3 +37,27 @@ vmod_rtstatus(VRT_CTX)
 	VSM_Delete(vd);
 	return "";
 }
+
+VCL_STRING
+vmod_html(const struct vrt_ctx *ctx)
+{
+	char *p;
+	unsigned u,v;
+
+        if (ctx->method != VCL_MET_SYNTH) {
+                VSLb(ctx->vsl, SLT_VCL_Error, "rtstatus() can only be used in vcl_synth");
+                return "{ \"error\": \"Check Varnishlog for more details\" }";
+        }
+
+	u = WS_Reserve(ctx->ws, 0);
+	p = ctx->ws->f;
+	v = snprintf(p, u, "%s", html);
+	v++;
+	if (v > u) {
+	    WS_Release(ctx->ws, 0);
+	    return "{ \"error\": \"Workspace overflow\" }";
+	}
+	WS_Release(ctx->ws, v);
+	return (p);
+}
+
